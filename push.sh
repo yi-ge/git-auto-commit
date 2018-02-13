8c7525d8c7b0fcdfa073278999642ae19bcd668c
yige <a@wyr.me>
2018-02-13 13:12:31 +0800

    git auto commit

--- push.sh
+++ push.sh
@@ -1,1 +1,2 @@
-git pull && /root/.nvm/versions/node/v9.11.2/bin/node add.js && git commit -a -m 'git auto commit'
+/root/.nvm/versions/node/v9.11.2/bin/node add.js && git commit -a -m 'git auto commit'
+
